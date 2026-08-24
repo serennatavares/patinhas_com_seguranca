c99879212474414f2eb9efac28b11d95bb9f27be
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-08-24 11:04:32 -0300

    fix - database

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+include("infra/conexao.php");
+?>
+
+<DOCTYPE!html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">    
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AuMigos</title>
+        <link rel="stylesheet" href="style/style.css">
+    </head>
+    <body>
+
+    <h1>Bem-vindo ao AuMigos!</h1>
+    <h2> Cadastre-se aqui:<h2>
+
+    <div>
+
+    <form action=public/cadastrar_clientes.php method="POST">
+    
+
+</div>
