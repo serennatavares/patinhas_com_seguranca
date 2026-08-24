fe1a0118eb7c250f70bc56b321af6beb14ed70f3
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-08-24 11:14:48 -0300

    fix - database

--- database/db.sql
+++ database/db.sql
@@ -4,8 +4,7 @@ USE au_migos;
 CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    telefone VARCHAR(15) NOT NULL
+    email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE animais (
