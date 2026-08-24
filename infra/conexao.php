17329f767a9cd77a67b21948a91c9f8be22259ef
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-08-24 10:53:42 -0300

    add - database e conexão

--- infra/conexao.php
+++ infra/conexao.php
@@ -5,6 +5,12 @@ $usuario = "root";
 $senha = "";
 $banco = "au_migos";
 
-$conn = new mysqli($host, $S)
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
 
 ?>
