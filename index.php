e749f1362452bdbf27ba38d4e040bfba75d92dcf
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-08-24 12:03:37 -0300

    fix - index

--- index.php
+++ index.php
@@ -1,33 +1,38 @@
 <?php 
 include("infra/conexao.php");
+
+$sql = "SELECT * FROM clientes";
+$clientes = $conn->query($sql);
+
 ?>
 
-<DOCTYPE!html>
+<!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">    
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>AuMigos</title>
         <link rel="stylesheet" href="style/style.css">
     </head>
+
     <body>
 
     <h1>Bem-vindo ao AuMigos!</h1>
-    <h2> Cadastre-se aqui:<h2>
+    <h2> Cadastre-se aqui:</h2>
 
     <div>
 
-        <form action=public/cadastrar_clientes.php method="POST">
+        <form action="public/cadastrar_clientes.php" method="POST">
+
             <label for="nome">Nome:</label>
             <input type="text" name="nome" required>
 
             <label for="email">Email:</label>
             <input type="email" name="email" required>
 
-            <button type=submit>Cadastrar</button>
+            <button type="submit">Cadastrar</button>
         </form>
 
-        <a href="public/listar_clientes.php">Listar Clientes</a>
+        <a href="public/listar_animais.php">Listar Animais</a>
 
     </div>
 
@@ -38,27 +43,23 @@ include("infra/conexao.php");
             <th>Email</th>
         </tr>
 
-    <?php
 
-        $sql = "SELECT * FROM clientes";
-
-        $clientes = $conn->query($sql);
-
-        while ($clientes = $clientes->fetch_assoc($clientes)) { ?>
-
-        ?>
+        <?php while ($cliente = $clientes->fetch_assoc()) { ?>
 
             <tr>
-                <td><?php echo $clientes['id']; ?></td>
-                <td><?php echo $clientes['nome']; ?></td>
-                <td><?php echo $clientes['email']; ?></td>
+                <td><?php echo $cliente['id']; ?></td>
+                <td><?php echo $cliente['nome']; ?></td>
+                <td><?php echo $cliente['email']; ?></td>
+
                 <td>
-                    <a href="public/cadastrar_animais.php?id=<?php echo $clientes['id']; ?>">Cadastrar Animal</a>
-                    <a href="public/listar_animais.php?id=<?php echo $clientes['id']; ?>">Listar Animais</a>
+                    <a href="public/cadastrar_animais.php?id=<?php echo $cliente['id']; ?>">Cadastrar Animal</a>
+                    <a href="public/listar_animais_usuario.php?id=<?php echo $cliente['id']; ?>">Listar Animais do Usuário</a>
                 </td>
+
             </tr>
 
-    <?php } ?>
+        <?php } ?>
+    </table>
 
 </body>
 </html>
